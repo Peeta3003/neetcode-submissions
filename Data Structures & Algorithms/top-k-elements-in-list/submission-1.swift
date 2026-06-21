b6f681f73e90aa6fdaf32433b6f8ab2e1a076054
Petr <79222528+Peeta3003@users.noreply.github.com>
2026-06-21 10:21:48 +0200

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.swift
@@ -0,0 +1,11 @@
+class Solution {
+    func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
+        var numMap: [Int: Int] = [:]
+
+        for num in nums {
+            numMap[num, default: 0] += 1
+        }
+
+        return numMap.sorted { $0.value > $1.value }.prefix(k).map { $0.key }
+    }
+}
