3bed3ffe77cc77a9b7978bb457180641ca8e6dd5
Petr <79222528+Peeta3003@users.noreply.github.com>
2026-06-25 20:30:02 +0200

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.swift
@@ -0,0 +1,11 @@
+class Solution {
+    func getConcatenation(_ nums: [Int]) -> [Int] {
+        var ans: [Int] = nums
+
+        for num in nums {
+            ans.append(num)
+        }
+
+        return ans
+    }
+}
